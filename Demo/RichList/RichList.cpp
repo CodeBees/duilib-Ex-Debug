8c79d5f2c46c501c7f8c9fbb354e5bd1cb541bb5
scenic <x356982611@gmail.com>
2014-09-25 16:36:20 +0800

    加入虚拟窗口的例子，

--- Demo/RichList/RichList.cpp
+++ Demo/RichList/RichList.cpp
@@ -56,7 +56,18 @@ DUI_BEGIN_MESSAGE_MAP(CRichListWnd, WindowImplBase)
 	DUI_END_MESSAGE_MAP()
 
 //////////////////////////////////////////////////////////////////////////
-	LPCTSTR CRichListWnd::GetWindowClassName( void ) const
+CRichListWnd::CRichListWnd()
+{
+	m_Page1.SetPaintMagager(&m_PaintManager);
+	AddVirtualWnd(_T("page1"),&m_Page1);
+}
+
+CRichListWnd::~CRichListWnd()
+{
+	RemoveVirtualWnd(_T("page1"));
+}
+
+LPCTSTR CRichListWnd::GetWindowClassName( void ) const
 {
 	return _T("RichListWnd");
 }
@@ -144,4 +155,113 @@ void CRichListWnd::OnSelectChanged( TNotifyUI &msg )
 	{
 		static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("tab_main")))->SelectItem(1);
 	}
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+///
+
+
+DUI_BEGIN_MESSAGE_MAP(CPage1, CNotifyPump)
+	DUI_ON_MSGTYPE(DUI_MSGTYPE_CLICK,OnClick)
+	DUI_ON_MSGTYPE(DUI_MSGTYPE_SELECTCHANGED,OnSelectChanged)
+	DUI_ON_MSGTYPE(DUI_MSGTYPE_ITEMCLICK,OnItemClick)
+	DUI_END_MESSAGE_MAP()
+
+	CPage1::CPage1()
+{
+	m_pPaintManager = NULL;
+}
+
+void CPage1::SetPaintMagager(CPaintManagerUI* pPaintMgr)
+{
+	m_pPaintManager = pPaintMgr;
+}
+
+void CPage1::OnClick(TNotifyUI& msg)
+{
+	if(msg.pSender->GetName() == _T("down_ico"))
+	{                
+		CControlUI *find_ctrl =m_pPaintManager->FindSubControlByName(msg.pSender->GetParent()->GetParent(), _T("down_name"));
+
+		if(find_ctrl)
+		{
+			MessageBox(NULL, 
+				find_ctrl->GetText()+_T(" 演示未选中行中的按钮触发动作，依该按钮父结点的找到所属行listcontainer.."), 
+				_T("DUILIB DEMO"), MB_OK);   
+			((CLabelUI *)find_ctrl)->SetText(_T("由程序动态设置后的名称..."));
+		}
+	}
+	else if(msg.pSender->GetName() == _T("down_del"))
+	{
+		CListUI *down_list = 
+			static_cast<CListUI*>(m_pPaintManager->FindControl(_T("down_list_tab")));
+		if(!down_list)
+			return;
+
+		down_list->RemoveAt(down_list->GetCurSel());                   
+	}
+	else if(msg.pSender->GetName() == _T("down_new"))
+	{
+		CListUI *down_list = static_cast<CListUI*>(m_pPaintManager->FindControl(_T("down_list_tab")));
+		if(!down_list)
+			return;
+
+		CListContainerElementUI *new_node = new CListContainerElementUI;
+		new_node->ApplyAttributeList(_T("height=\"45\""));
+
+		CHorizontalLayoutUI *new_h_lay = new CHorizontalLayoutUI;
+		new_h_lay->ApplyAttributeList(_T("float=\"false\" ")\
+			_T("childpadding=\"10\" inset=\"3,5,3,5\""));
+
+		CButtonUI *new_btn_1 = new CButtonUI;
+		new_btn_1->ApplyAttributeList(
+			_T("name=\"down_ico\" float=\"false\" ")\
+			_T("bordersize=\"0\" width=\"32\" maxheight=\"26\" ")\
+			_T("bkimage=\"downlist_app.png\" ")\
+			_T("normalimage=\"file='downlist_run.png' dest='20,14,32,26'\""));
+
+		CVerticalLayoutUI *new_v_lay = new CVerticalLayoutUI;
+		new_h_lay->Add(new_btn_1);
+		new_h_lay->Add(new_v_lay);
+
+		CLabelUI *new_label = new CLabelUI;
+		new_label->ApplyAttributeList(_T("textcolor=\"#FFAAAAAA\" showhtml=\"true\""));
+		new_label->SetText(_T("new added item.exe"));
+		new_label->SetName(_T("down_name"));
+		CProgressUI *new_progress = new CProgressUI;
+		new_progress->SetMinValue(0);
+		new_progress->SetMaxValue(100);
+		new_progress->SetValue(1);
+		new_progress->SetMaxWidth(200);
+		new_progress->SetMaxHeight(7);
+		new_progress->SetForeImage(_T("progress_fore.png"));
+		new_progress->SetName(_T("down_progress"));
+		new_v_lay->Add(new_label);
+		new_v_lay->Add(new_progress);
+
+		CLabelUI *new_label2 = new CLabelUI;
+		CLabelUI *new_label3 = new CLabelUI;
+		CVerticalLayoutUI *new_v_lay2 = new CVerticalLayoutUI;
+		new_h_lay->Add(new_v_lay2);
+		new_v_lay2->Add(new_label2);
+		new_v_lay2->Add(new_label3);
+		new_label2->ApplyAttributeList(
+			_T("align=\"right\" text=\"\" textcolor=\"#FFAAAAAA\" showhtml=\"true\""));
+		new_label3->ApplyAttributeList(
+			_T("align=\"right\" text=\"0.00K/34.33M \" textcolor=\"#FFAAAAAA\" showhtml=\"true\""));
+
+		new_node->Add(new_h_lay);
+		down_list->Add(new_node);
+	}
+}
+
+void CPage1::OnSelectChanged( TNotifyUI &msg )
+{
+
+}
+
+void CPage1::OnItemClick( TNotifyUI &msg )
+{
+
 }
--- Demo/RichList/RichList.h
+++ Demo/RichList/RichList.h
@@ -4,11 +4,33 @@
 #pragma  once
 #include "stdafx.h"
 
+
+
+
+//////////////////////////////////////////////////////////////////////////
+///
+
+class CPage1 : public CNotifyPump
+{
+public:
+	CPage1();
+	void SetPaintMagager(CPaintManagerUI* pPaintMgr);
+
+	DUI_DECLARE_MESSAGE_MAP()
+		virtual void OnClick(TNotifyUI& msg);
+	virtual void OnSelectChanged( TNotifyUI &msg );
+	virtual void OnItemClick( TNotifyUI &msg );
+private:
+	CPaintManagerUI* m_pPaintManager;
+};
+
+//////////////////////////////////////////////////////////////////////////
+
 class CRichListWnd : public WindowImplBase
 {
 public:
-	CRichListWnd(void){};
-	virtual ~CRichListWnd(void){};
+	CRichListWnd(void);
+	virtual ~CRichListWnd(void);
 
 	virtual void OnFinalMessage( HWND );
 	virtual CDuiString GetSkinFolder(){
@@ -34,8 +56,9 @@ private:
 	CButtonUI* m_pMaxBtn;
 	CButtonUI* m_pRestoreBtn;
 	CButtonUI* m_pMinBtn;
-	//CPage1 m_Page1;
-	//CPage2 m_Page2;
+	CPage1 m_Page1;
+	
 };
+
 
 
